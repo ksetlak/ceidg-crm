910b007a04fe5746e21a93346fee4b03d43f26c3
Krzysiek <bigfoot19@wp.pl>
2024-03-14 01:46:48 +0100

    Finished the packaging script.

--- setup.py
+++ setup.py
@@ -2,18 +2,27 @@ from cx_Freeze import setup, Executable
 
 VERSION = "0.1.0"
 
-msi_options = dict(
-    target_name=f'CEiDG-CRM-{VERSION.replace(".", "_")}.msi',
-    upgrade_code='{fdb31120-1b7f-4c35-94e3-d7cafcd4ee23}'  # Thanks https://www.guidgenerator.com/online-guid-generator.aspx!
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {"packages": [], "excludes": []}
+bdist_msi_options = dict(
+    target_name=f"CEiDG-CRM-{VERSION.replace('.', '_')}.msi",
+    upgrade_code="{fdb31120-1b7f-4c35-94e3-d7cafcd4ee23}"
+    # Thanks https://www.guidgenerator.com/online-guid-generator.aspx!
 )
 
-setup(name='CEiDG-CRM',
-      version=VERSION,
-      description='A PoC for software you can use to contact newly registered sole proprietorships in Poland to offer them your services.',
-      executables=[Executable('ceidg-crm.pyw',
-                              base='Win32GUI',
-                              targetName='ceidg-crm.exe',
-                              # icon='assets/inverter.ico',
-                              shortcutName='CEiDG-CRM',
-                              shortcutDir='StartMenuFolder'
-                              )])
+base = 'Win32GUI'
+
+executables = [
+    Executable("main.py",
+               base=base,
+               target_name="CEiDG-CRM",
+               shortcut_name="CEiDG-CRM",
+               shortcut_dir="StartMenuFolder")
+]
+
+setup(name="CEiDG-CRM",
+      version="0.1.0",
+      description="A PoC for software that lets you discover newly registered sole proprietorships so that you can offer them your services.",
+      options={"build_exe": build_options, "bdist_msi": bdist_msi_options},
+      executables=executables)
